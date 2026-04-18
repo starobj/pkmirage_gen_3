fdceb11e5227ae68bf7f507dac7e935b3b810540
starobj <starobj@starobj.com>
2026-04-18 18:57:16 -0500

    Update contest config

--- include/config/contest.h
+++ include/config/contest.h
@@ -2,7 +2,7 @@
 #define GUARD_CONFIG_CONTEST_H
 
 // Move data settings
-#define C_UPDATED_MOVE_CATEGORIES   GEN_LATEST // Updates contest category.
-#define C_UPDATED_MOVE_EFFECTS      GEN_LATEST // Updates contest effects.
+#define C_UPDATED_MOVE_CATEGORIES   GEN_6      // Updates contest category.
+#define C_UPDATED_MOVE_EFFECTS      GEN_6      // Updates contest effects.
 
 #endif // GUARD_CONFIG_CONTEST_H
